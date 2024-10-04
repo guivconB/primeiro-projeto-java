46671ed0f51149e5ae402dc98268d1c38cec0f07
Guilherme Vieira <67739314+guivconB@users.noreply.github.com>
2024-10-04 17:10:31 -0300

    Add files via upload

---
+++ src/teste/Main.java
@@ -0,0 +1,16 @@
+package teste;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner teclado = new 	Scanner(System.in);
+		System.out.println("Digite um valor inteiro:");
+		int valor;
+		valor = teclado.nextInt();
+		System.out.println("Valor digitado: " + valor );
+
+	}
+
+}
